2690de054a9fe7e6e6106a7d8f45b8b16ee47109
bswan0002 <bswan0002@gmail.com>
2021-02-12 18:35:53 -0800

    last save before boss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 import WineNav from "./components/WineNav";
 import GuiPage from "./components/GuiPage";
 import Bottles from "./components/Bottles";
