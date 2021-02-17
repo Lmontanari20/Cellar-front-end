28d299ffe4c21ec4efa7a365937ca9328a570226
Lucas Montanari <Lmonty2011@gmail.com>
2021-02-17 14:56:25 -0800

    added more attributes to filter from

--- src/components/cellar-forms/Filter.js
+++ src/components/cellar-forms/Filter.js
@@ -1,4 +1,5 @@
 import { Form, Row, Col, Button, Container } from "react-bootstrap";
+import NumericInput from "react-numeric-input";
 import React from "react";
 
 export default class Filter extends React.Component {
@@ -21,6 +22,29 @@ export default class Filter extends React.Component {
     return types.map((type) => <option key={type}>{type}</option>);
   };
 
+  sizeOptions = () => {
+    const sizes = [
+      "Standard (750ml)",
+      "Split/Piccolo (187.5ml)",
+      "Half/Demi (375 ml)",
+      "Half-liter/Jennie (500ml)",
+      "Liter (1000ml)",
+      "Magnum (1.5 L)",
+      "Double Magnum (3 L)",
+      "Rehoboam (Jeroboam in Bordeaux) (4.5 L)",
+      "Methuselah or Imperial (Bordeaux) (6 L)",
+      "Salmanazar (9 L)",
+      "Balthazar (12 L)",
+      "Nebuchadnezzar (15 L)",
+      "Melchior (18 L)",
+      "Solomon (20 L)",
+      "Sovereign (26 L)",
+      "Primat/Goliath (27 L)",
+      "Melchizedek/Midas (30 L)",
+    ];
+    return sizes.map((size) => <option key={size}>{size}</option>);
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
     let filteredBottles = [];
@@ -40,6 +64,17 @@ export default class Filter extends React.Component {
       if (e.target.type.value !== "") {
         this.props.sections[i].bottles.forEach((bottle) => {
           bottle.wine.wineType === e.target.type.value &&
+            filteredBottles.push(bottle);
+        });
+      }
+      if (e.target.size.value !== "") {
+        this.props.sections[i].bottles.forEach((bottle) => {
+          bottle.size === e.target.size.value && filteredBottles.push(bottle);
+        });
+      }
+      if (e.target.year.value !== "") {
+        this.props.sections[i].bottles.forEach((bottle) => {
+          bottle.wine.year === parseInt(e.target.year.value) &&
             filteredBottles.push(bottle);
         });
       }
@@ -79,6 +114,28 @@ export default class Filter extends React.Component {
                 <Form.Control name="type" as="select">
                   {this.selectOptions()}
                 </Form.Control>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="mt-2">
+            <Col>
+              <Form.Group>
+                <Form.Label>Size</Form.Label>
+                <Form.Control name="size" as="select">
+                  {this.sizeOptions()}
+                </Form.Control>
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group>
+                <Form.Label>Year</Form.Label>
+                <NumericInput
+                  className="form-control"
+                  value={2016}
+                  max={2021}
+                  min={300}
+                  name="year"
+                ></NumericInput>
               </Form.Group>
             </Col>
             <Col>
