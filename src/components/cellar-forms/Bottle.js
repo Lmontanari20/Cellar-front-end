dfbccd679adcfbcac3d29886b02e692e41a3a938
bswan0002 <bswan0002@gmail.com>
2021-02-18 09:26:11 -0800

    bottle is now cc

--- src/components/cellar-forms/Bottle.js
+++ src/components/cellar-forms/Bottle.js
@@ -1,29 +1,32 @@
+import React, { Component } from "react";
 import { Form, Row, Col, Button, Container } from "react-bootstrap";
 import NumericInput from "react-numeric-input";
 import CurrencyInput from "react-currency-input-field";
 import WineTypes from "../../WineTypes";
 import WineSizes from "../../WineSizes";
 
-const Bottle = (props) => {
-  const sizeOptions = () => {
+class Bottle extends Component {
+  state = {};
+
+  sizeOptions = () => {
     const sizes = Object.keys(WineSizes);
     return sizes.map((size) => <option key={size}>{size}</option>);
   };
 
-  const typeOptions = () => {
+  typeOptions = () => {
     const types = Object.keys(WineTypes);
     return types.map((type) => <option key={type}>{type}</option>);
   };
 
-  const sections = () => {
-    if (props.sections) {
-      return props.sections.map((section) => {
+  sections = () => {
+    if (this.props.sections) {
+      return this.props.sections.map((section) => {
         return <option key={section.name}>{section.name}</option>;
       });
     }
   };
 
-  const handleSubmit = (e) => {
+  handleSubmit = (e) => {
     e.preventDefault();
 
     let price = parseInt(e.target.price.value.substring(1).replaceAll(",", ""));
@@ -42,97 +45,97 @@ const Bottle = (props) => {
       section: e.target.section.value,
     };
 
-    props.bottleSubmit(wine, bottle);
+    this.props.bottleSubmit(wine, bottle);
     e.target.reset();
   };
 
-  // tooltips on the form might be cool
-  // maybe Formik for validation and handling input values
-  return (
-    <div className="form-div">
-      <Container>
-        <Form onSubmit={handleSubmit}>
-          <Row>
-            <Col>
-              <Form.Group>
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter Wine Name"
-                  name="name"
-                ></Form.Control>
-                <Form.Label className="mt-2">Winery</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter Winery"
-                  name="winery"
-                ></Form.Control>
-                <Form.Label className="mt-2">Section</Form.Label>
-                <Form.Control as="select" name="section">
-                  {sections()}
-                </Form.Control>
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group>
-                <Form.Label>Type</Form.Label>
-                <Form.Control name="type" as="select">
-                  {typeOptions()}
-                </Form.Control>
-                <Form.Label className="mt-2">Year</Form.Label>
-                <NumericInput
-                  className="form-control"
-                  value={2016}
-                  max={2021}
-                  min={300}
-                  name="year"
-                ></NumericInput>
-                <Form.Row className="mt-2">
-                  <Col>
-                    <Form.Label>Row Number</Form.Label>
-                    <Form.Control
-                      type="text"
-                      placeholder="X Coordinate"
-                      name="row"
-                    ></Form.Control>
-                  </Col>
-                  <Col>
-                    <Form.Label>Column Number</Form.Label>
-                    <Form.Control
-                      type="text"
-                      placeholder="Y Coordinate"
-                      name="column"
-                    ></Form.Control>
-                  </Col>
-                </Form.Row>
-              </Form.Group>
-            </Col>
-            <Col className="butt-col">
-              <Form.Group>
-                <Form.Label>Size</Form.Label>
-                <Form.Control name="size" as="select">
-                  {sizeOptions()}
-                </Form.Control>
-                <Form.Label className="mt-2">Price</Form.Label>
-                <CurrencyInput
-                  className="form-control"
-                  name="price"
-                  placeholder="$12.34"
-                  prefix="$"
-                  decimalsLimit={2}
-                ></CurrencyInput>
-              </Form.Group>
-              <div className="mt-4 butt-div" style={{ textAlign: "right" }}>
-                <Button variant="primary" type="submit">
-                  Submit
-                </Button>
-              </div>
-            </Col>
-          </Row>
-        </Form>
-      </Container>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="form-div">
+        <Container>
+          <Form onSubmit={this.handleSubmit}>
+            <Row>
+              <Col>
+                <Form.Group>
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter Wine Name"
+                    name="name"
+                  ></Form.Control>
+                  <Form.Label className="mt-2">Winery</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter Winery"
+                    name="winery"
+                  ></Form.Control>
+                  <Form.Label className="mt-2">Section</Form.Label>
+                  <Form.Control as="select" name="section">
+                    {this.sections()}
+                  </Form.Control>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group>
+                  <Form.Label>Type</Form.Label>
+                  <Form.Control name="type" as="select">
+                    {this.typeOptions()}
+                  </Form.Control>
+                  <Form.Label className="mt-2">Year</Form.Label>
+                  <NumericInput
+                    className="form-control"
+                    value={2016}
+                    max={2021}
+                    min={300}
+                    name="year"
+                  ></NumericInput>
+                  <Form.Row className="mt-2">
+                    <Col>
+                      <Form.Label>Row Number</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="X Coordinate"
+                        name="row"
+                      ></Form.Control>
+                    </Col>
+                    <Col>
+                      <Form.Label>Column Number</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Y Coordinate"
+                        name="column"
+                      ></Form.Control>
+                    </Col>
+                  </Form.Row>
+                </Form.Group>
+              </Col>
+              <Col className="butt-col">
+                <Form.Group>
+                  <Form.Label>Size</Form.Label>
+                  <Form.Control name="size" as="select">
+                    {this.sizeOptions()}
+                  </Form.Control>
+                  <Form.Label className="mt-2">Price</Form.Label>
+                  <CurrencyInput
+                    className="form-control"
+                    name="price"
+                    placeholder="$12.34"
+                    prefix="$"
+                    decimalsLimit={2}
+                  ></CurrencyInput>
+                </Form.Group>
+                <div className="mt-4 butt-div" style={{ textAlign: "right" }}>
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                </div>
+              </Col>
+            </Row>
+          </Form>
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default Bottle;
